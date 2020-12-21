dbca4785423cfb67af1a2e71eff4e1f96e51098f
Hernán <hernan@hego.com.ar>
2020-12-21 20:06:32 -0300

    Reemplace las tarifas del regional por el nacional

--- includes/tarifas.php
+++ includes/tarifas.php
@@ -10,12 +10,12 @@ $envioGratis = 200000;
 
 ////////////////////////////// TARIFAS A DOMICILIO REGIONAL	////////////////////
 $domicilioTarifaRegional = array(
-	1 => 430,
-	5 => 520,
-	10 => 655,
-	15 => 795,
-	20 => 995,
-	25 => 1155,
+	1 => 595,
+	5 => 735,
+	10 => 975,
+	15 => 1205,
+	20 => 1410,
+	25 => 1690,
 	'adicional' => 53
 );	
 ////////////////////////////// TARIFAS A DOMICILIO NACIONAL	////////////////////
@@ -30,12 +30,12 @@ $domicilioTarifaNacional = array(
 );
 ////////////////////////////// TARIFAS A SUCURSAL REGIONAL	////////////////////
 $sucursalTarifaRegional = array(
-	1 => 385,
-	5 => 475,
-	10 => 610,
-	15 => 750,
-	20 => 950,
-	25 => 1110,
+	1 => 550,
+	5 => 690,
+	10 => 930,
+	15 => 1160,
+	20 => 1365,
+	25 => 1645,
 	'adicional' => 53
 );	
 ////////////////////////////// TARIFAS A SUCURSAL NACIONAL	////////////////////
